773dccb87b1c679c148e0c371beb7aac4a3fca3f
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r7p3.42sp.org.br>
2024-11-06 13:59:52 -0300

    baaaaaaaaaa

--- ft_printf.c
+++ ft_printf.c
@@ -3,40 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 20:08:55 by gabriel           #+#    #+#             */
-/*   Updated: 2024/11/05 19:33:16 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/06 12:43:39 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-static int	ft_verify_format(const char identifier)
+static int	ft_verify_format(const char *id, size_t to_print, int final_count)
 {
-	char	identifier;
-
-	if (identifier == 'c')
-		ft_putchar_fd();
-	// if (identifier == 's')
-	// 	ft_putstr_fd();
-	// if (identifier == 'p')
-	// 	return (aaa);
-	// if (identifier == 'd')
-	// 	ft_putnbr_fd();
-	// if (identifier == 'i')
-	// 	ft_putnbr_fd();
-	// if (identifier == 'u')
-	// 	ft_putnbr_fd();
-	// if (identifier == 'x')
-	// 	//dsahdsaudsa
-	// if (identifier == 'X')
-	// 	//uidagsadsa
-	// if (identifier == '%')
-	// 	ft_putchar_fd('%');
+	if (*id[1] == 'c')
+		ft_putchar(to_print, to_print);
+	return (final_count);
 }
 
 int	ft_printf(const char *format, ...)
-{
-	
+{	
+	char	id;
+	size_t	to_print;
+	int		potato;
+
+	ft_verify_format(format, to_print, potato);
+	return (0);
 }
+
+int	main(void)
+{
+	char	potato = 'a';
+
+	ft_printf("%c\n", potato);
+	//printf("%c\n", potato);
+	return (0);
+}
+
+/*	if (id == 's')
+		ft_putstr();
+	if (id == 'p')
+		return (aaa);
+	if (id == 'd')
+		ft_putnbr();
+	if (id == 'i')
+		ft_putnbr();
+	if (id == 'u')
+		ft_putnbr();
+	if (id == 'x')
+		//dsahdsaudsa
+	if (id == 'X')
+		//uidagsadsa
+	if (id == '%')
+		ft_putchar('%');*/
--- ft_printf.h
+++ ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:01:40 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/11/05 19:49:01 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/06 12:10:32 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 void	ft_putstr(char *s);
-void	ft_putchar(char c);
+void	ft_putchar(char c, size_t final_count);
 void	ft_putnbr(int n);
 
 #endif
