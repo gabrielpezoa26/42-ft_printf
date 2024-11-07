00e2cda5101ecd88445f752388c2d2c751aa1c3e
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r8p4.42sp.org.br>
2024-11-07 16:06:44 -0300

    com %i ta funfandoooooooooooo

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 11:11:20 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/11/07 15:09:11 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/11/07 16:04:02 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,15 @@
 int	ft_putnbr(int n)
 {
 	long	num;
-	int		k;
 	
 	num = (long)n;
-	k = 0;
-	while (num[k] != '\0')
+	if (num < 0)
 	{
-		if (num[k] < 0)
-		{
-			write(1, "-", 1);
-			num = -num;
-			k++;
-		}
-		if (num[k] > 9)
-			ft_putnbr(num / 10);
-			
-		write(1, &((char){num % 10 + '0'}), 1);
-		k++;
+		write(1, "-", 1);
+		num = -num;
 	}
+	if (num > 9)
+		ft_putnbr(num / 10);
+	write(1, &((char){num % 10 + '0'}), 1);
+	return (1);
 }
