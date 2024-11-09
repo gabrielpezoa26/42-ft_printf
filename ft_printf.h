58396c4d29a5e41e75a2c7a1d8c86d7000744740
Gabriel <gabrielpezoa26@gmail.com>
2024-11-09 15:59:47 -0300

    id u corrigidoooo

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:01:40 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/11/08 18:21:52 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/09 15:56:13 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int		ft_printf(const char *format, ...);
 int		ft_putstr(char *s);
 int		ft_putchar(char c);
 int		ft_putnbr(int n);
-int		ft_putunbr(int n);
+int		ft_putunbr(unsigned int n);
 int		ft_putptr(void *y);
 int		ft_print_hex_upper(unsigned long n);
 int		ft_print_hex_lower(unsigned long n);
--- ft_putunbr.c
+++ ft_putunbr.c
@@ -6,24 +6,21 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 16:20:01 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/11/08 18:32:15 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/09 15:55:26 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putunbr(int n)
+int	ft_putunbr(unsigned int n)
 {
 	long	num;
+	int		count;
 
 	num = (long)n;
-	if (num >= 0)
-	{
-		if (num > 9)
-			ft_putnbr(num / 10);
-		write(1, &((char){num % 10 + '0'}), 1);
-	}
-	else if (num <= 0)
-		return (0);
-	return (1);
+	count = 0;
+	if (num > 9)
+		count += ft_putunbr(num / 10);
+	count += ft_putchar(num % 10 + '0');
+	return (count);
 }
