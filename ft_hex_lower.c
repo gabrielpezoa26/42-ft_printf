58ee8de931a41031bbb55a3ef3fcb772ca6cb798
Gabriel <gabrielpezoa26@gmail.com>
2024-11-09 19:27:02 -0300

    hex lower nomenclatura corrigida

--- ft_hex_lower.c
+++ ft_hex_lower.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 17:33:48 by gabriel           #+#    #+#             */
-/*   Updated: 2024/11/08 18:28:06 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/09 19:23:12 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 int	ft_print_hex_lower(unsigned long n)
 {
 	char	*hex_digits_lower;
-	int		chaos_upper;
+	int		result;
 
-	chaos_upper = 0;
+	result = 0;
 	hex_digits_lower = "0123456789abcdef";
 	if (n >= 16)
-		chaos_upper += ft_print_hex_lower(n / 16);
-	chaos_upper += ft_putchar(hex_digits_lower[n % 16]);
-	return (chaos_upper);
+		result += ft_print_hex_lower(n / 16);
+	result += ft_putchar(hex_digits_lower[n % 16]);
+	return (result);
 }
