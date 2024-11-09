513db7bf315324d92aaba782413d35f26b361188
Gabriel <gabrielpezoa26@gmail.com>
2024-11-09 00:12:28 -0300

    id p quase funfandoo

--- ft_putptr.c
+++ ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:32:06 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/11/08 18:26:16 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/09 00:11:13 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,11 @@ static int	caneta_azul(unsigned long n)
 
 int	ft_putptr(void *y)
 {
-	unsigned long	mango_loko;
 	int				count;
 
-	count = 0;
-	mango_loko = (unsigned long)y;
-	count += ft_putstr("0x");
-	if (mango_loko == 0)
-		count += ft_putchar('0');
-	else
-		count += caneta_azul(mango_loko);
+	if (y == 0)
+		return (ft_putstr("0x0"));
+	count = ft_putstr("0x");
+	count += caneta_azul((unsigned long)y);
 	return (count);
 }
